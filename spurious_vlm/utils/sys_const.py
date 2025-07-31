fc5ce7fd47d39fdb90e8fdf8e02e644462e8ca55
Wenqian Ye <wenqian@virginia.edu>
2025-07-30 22:23:34 -0700

    for submission

--- spurious_vlm/utils/sys_const.py
+++ spurious_vlm/utils/sys_const.py
@@ -1,4 +1,4 @@
-DATA_DIR = '/home/ubuntu/wqyeh-workspace/spurious_prompt_vlm/datasets'
-ROOT_DIR = '/home/ubuntu/wqyeh-workspace/spurious_prompt_vlm/spurious_vlm'
-MODEL_CACHE = '/home/ubuntu/wqyeh-workspace/spurious_prompt_vlm/spurious_vlm/model_cache'
-LLAMA_PATH = "/home/ubuntu/wqyeh-workspace/spurious_prompt_vlm/spurious_vlm/hf_llama"
+DATA_DIR = ''
+ROOT_DIR = ''
+MODEL_CACHE = ''
+LLAMA_PATH = ""
